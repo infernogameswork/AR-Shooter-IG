de105985366d299e15686743e08b1435cac47e77
infernogameswork <72456600+infernogameswork@users.noreply.github.com>
2020-10-21 21:02:22 +0200

    SCOPE

---
+++ Assets/SCRIPTS/SCOPE.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCOPE : MonoBehaviour
+{
+    public GameObject scopeOverlay;
+    public GameObject mainCamera;
+
+    public bool isScoped = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            scopeOverlay.SetActive(true);
+            mainCamera.GetComponent<Camera>().fieldOfView = 15;
+        }
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            scopeOverlay.SetActive(false);
+            mainCamera.GetComponent<Camera>().fieldOfView = 60;
+        }
+    }
+}
